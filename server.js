6f3e8783117bcdf66401f895cf1c96d1c5dd93b9
Abheet kumar <abheetkumar1999@gmail.com>
2021-06-28 19:27:10 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express();
+
+
+//created server
+const http = require('http').createServer(app)
+
+//created the port
+const port = process.env.PORT || 8000
+//listening the port
+http.listen(port,()=>{
+    console.log(`connected to the ${port}`)
+})
+//serving index.html 
+app.use(express.static(__dirname+'/public'))
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname+'/index.html');
+})
+
+//created the socket.io
+const io=require('socket.io')(http)
+io.on("connection",(socket)=>{
+    console.log('connected')
+    socket.on('message',(msg)=>{
+        socket.broadcast.emit('message',msg);
+    })
+})

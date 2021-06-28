7a8e8c608ee3441518472c3b30e359909e927a86
abheetkumar1999 <56573972+abheetkumar1999@users.noreply.github.com>
2021-06-28 19:55:47 +0530

    update

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ http.listen(port,()=>{
 //serving index.html 
 app.use(express.static(__dirname+'/public'))
 app.get('/', (req,res)=>{
-    res.sendFile(__dirname+'/public/index.html');
+    res.sendFile(__dirname+'/index.html');
 })
 
 //created the socket.io
